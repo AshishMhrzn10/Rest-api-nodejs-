a48d3e1e57eb3794649f513c3094bf5dd7ad3647
Ashish Maharjan <amaharjan1032@gmail.com>
2020-12-02 10:52:58 +0545

    CRUD performed

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require("body-parser");
+require("dotenv/config");
+
+app.use(bodyParser.json());
+
+//Import routes
+const postRoute = require("./routes/posts");
+app.use("/posts", postRoute);
+
+//ROUTES
+app.get("/", (req, res) => {
+	res.send("We are on home");
+});
+
+//Connect mongodb
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+	console.log("Connected to DB");
+});
+
+//Start server
+app.listen(3000);
